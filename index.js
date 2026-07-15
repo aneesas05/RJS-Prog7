cdc1d3f92f24f7b37f7ef12732325b46e55803a5
aneesas05 <uchihamadara0087@gmail.com>
2026-07-15 10:15:30 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,5 +4,5 @@
 // Text: Welcome to React
 
 const element= React.createElement("h1","welcome to react");
-const root = ReactDom.createRoot(doucument.getElementById("root"));
+const root = ReactDom.createRoot(doucument.getElementById("Root"));
 root.render(element);
