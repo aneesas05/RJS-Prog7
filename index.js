443fbba526817110a613f986e6b38ee73329f7c6
aneesas05 <uchihamadara0087@gmail.com>
2026-07-15 09:59:57 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,6 @@
 // Tag name: h1
 // Text: Welcome to React
 
-const element= Recat.createElement("h1",nulll,"welcome to my web page");
+const element= Recat.createElement(,nulll,<h1>"welcome to my web page"</h1>);
 const root = ReactDom.createRoot(doucument.getElementById);
 root.render(element);
